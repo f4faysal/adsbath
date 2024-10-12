2feb99f934041301877d83d2740edb9d14d96e40
Md Faysal Hossain <F4faysals@gmail.com>
2024-10-12 21:39:01 +0600

    add

--- app/blog/page.tsx
+++ app/blog/page.tsx
@@ -31,41 +31,41 @@ export const metadata: Metadata = {
 const blogPosts = [
   {
     slug: "digital-agency-guide",
-    image: "/agency.jpg",
+    image: "blog-image.png",
     title: "We Are Digital Agency",
     description:
       "What is a Facebook Advertising Agency and How to Choose One for Your Business?",
   },
   {
     slug: "facebook-ads-ecommerce",
-    image: "/ecommerce.jpg",
+    image: "/blog-image.png",
     title: "Facebook Ads for E-Commerce",
     description:
       "Facebook Ads for Ecommerce Full Guideline: 15 Best Strategies in 2024",
   },
   {
     slug: "facebook-marketing-guide",
-    image: "/marketing.jpg",
+    image: "/blog-image.png",
     title: "Facebook Marketing",
     description:
       "What is Facebook Marketing: A Complete Guide for any Business",
   },
   {
     slug: "seo-link-building",
-    image: "/seo.jpg",
+    image: "/blog-image.png",
     title: "SEO Link Building",
     description:
       "White Label Link Building Services – Choosing the Best One for Your Business In 2024",
   },
   {
     slug: "best-software-companies-bangladesh",
-    image: "/software.jpg",
+    image: "/blog-image.png",
     title: "Best Software Companies",
     description: "Top 20 Best Software Companies in Bangladesh (2024)",
   },
   {
     slug: "digital-agency-selection",
-    image: "/agency.jpg",
+    image: "/blog-image.png",
     title: "We Are Digital Agency",
     description:
       "What is a Facebook Advertising Agency and How to Choose One for Your Business?",
@@ -76,7 +76,7 @@ export default function BlogPage() {
   return (
     <div className="bg-[#080711] min-h-screen text-white mt-24">
       <div className="container mx-auto px-4 py-8">
-        <div className="relative w-full h-64 rounded-[24px] overflow-hidden mb-8">
+        <div className="relative w-full h-20 md:h-64 rounded-sm md:rounded-[24px] overflow-hidden mb-8">
           <Image
             src="/blog-banner.png"
             alt="Journey Through Ideas"
@@ -85,28 +85,29 @@ export default function BlogPage() {
             priority
           />
           <div className="absolute inset-0 bg-[#2a0d34] bg-opacity-40 flex items-center justify-center">
-            <h1 className="text-white text-2xl md:text-6xl font-bold mt-16 font-['Anton']">
+            <h1 className="text-white text-2xl md:text-6xl font-bold md:mt-16 font-['Anton']">
               JOURNEY THROUGH IDEAS
             </h1>
           </div>
         </div>
-
-        <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6">
+        <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-[30px]">
           {blogPosts.map((post, index) => (
-            <Link href={`/blog/${post.slug}`} key={index}>
-              <div className="bg-white text-black rounded-lg shadow-md hover:shadow-lg transform hover:scale-105 transition-transform duration-300 overflow-hidden">
-                <div className="relative h-40 w-full">
-                  <Image
-                    src={post.image}
-                    alt={post.title}
-                    layout="fill"
-                    objectFit="cover"
-                  />
-                </div>
-                <div className="p-4">
-                  <h2 className="text-lg font-semibold mb-2">{post.title}</h2>
-                  <p className="text-sm">{post.description}</p>
-                </div>
+            <Link
+              href={`/blog/${index}`}
+              key={index}
+              className="bg-[#16072c] text-white rounded-xl shadow-md hover:shadow-lg transform hover:scale-[1.02] transition-transform duration-300 overflow-hidden h-full p-6 "
+            >
+              <div className="relative h-52 w-full rounded-xl overflow-hidden">
+                <Image
+                  src={"/blog-image.png"}
+                  alt={post.title}
+                  layout="fill"
+                  objectFit="cover"
+                />
+              </div>
+              <div className="p-4">
+                <h2 className="text-lg font-semibold mb-2">{post.title}</h2>
+                <p className="text-sm text-gray-300">{post.description}</p>
               </div>
             </Link>
           ))}
